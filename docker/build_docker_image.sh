457257a9b432b533c976507cd03f0c01011e0a04
justin <justin@overwatchimaging.com>
2022-07-08 13:35:59 -0700

    update dockerfile to use  nvcr.io/nvidia/tensorflow:22.01-tf1-py3.  create_onnx.py failure still occurs.

--- docker/build_docker_image.sh
+++ docker/build_docker_image.sh
@@ -1,11 +1,11 @@
 set -x
 set -e
 
-docker pull nvcr.io/nvidia/tensorflow:22.06-tf2-py3
+docker pull nvcr.io/nvidia/tensorflow:22.01-tf1-py3
 
 cp ../TensorRT/samples/python/efficientdet/requirements.txt .
 
-docker build -t deploy_efficientdet_tensorrt .
+docker build -t deploy_efficientdet_tensorrt_tf1 .
 
 rm requirements.txt
 
--- docker/run_deploy_efficientdet_tensorrt.sh
+++ docker/run_deploy_efficientdet_tensorrt.sh
@@ -2,4 +2,4 @@ set -x
 set -e
 
 
-docker run --gpus all -it -v $PWD/../TensorRT:/TensorRT -v $PWD/../saved_model:/saved_model  -v $PWD/../deploy_efficientdet:/deploy_efficientdet --rm --workdir /deploy_efficientdet deploy_efficientdet_tensorrt ./deploy_efficientdet.sh
+docker run --gpus all -it -v $PWD/../TensorRT:/TensorRT -v $PWD/../saved_model:/saved_model  -v $PWD/../deploy_efficientdet:/deploy_efficientdet --rm --workdir /deploy_efficientdet deploy_efficientdet_tensorrt_tf1 ./deploy_efficientdet.sh
